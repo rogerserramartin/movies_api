f5cb02f13aac0df22977ac0ff4afefecfee3acf5
rogerserra92 <roger.serra92@gmail.com>
2021-11-22 21:30:12 +0100

    basic auth popup

---
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.demo.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration //this indicates that this class is a configuration one for SpringBoot
+@EnableWebSecurity // It allows Spring to find (it's a @Configuration and, therefore, @Component ) and automatically apply the class to the global WebSecurity .
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // press control + o to override
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic(); //basic authentication
+        //localhost:8080/api/v1/movies/{id} to see the popup
+    }
+}
