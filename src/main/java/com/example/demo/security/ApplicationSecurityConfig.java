3874c741e384063f7d24fb78832403380b77961d
rogerserra92 <roger.serra92@gmail.com>
2021-11-23 22:49:32 +0100

    password encoder

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -1,14 +1,27 @@
 package com.example.demo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration //this indicates that this class is a configuration one for SpringBoot
 @EnableWebSecurity // It allows Spring to find (it's a @Configuration and, therefore, @Component ) and automatically apply the class to the global WebSecurity .
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired //with this we don't need to code application = new ApplicationSec...()
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) { //this passwordencoder will be the same as the one in passwordconfig.java, which is BCrypt
+        this.passwordEncoder = passwordEncoder;
+    }
     // press control + o to override
 
     @Override
@@ -22,5 +35,20 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic(); //basic authentication
         //localhost:8080/api/v1/movies/{id} to see the popup
         //Basic dXNlcjpiZmU2NWVhOC1lZWJiLTQ1OGYtYmUyYS1lYWUxMjNkN2NhOTg=   Example of Basic64 Encryption
+    }
+
+    //
+    @Override
+    @Bean //this will be instantiated for us
+    protected UserDetailsService userDetailsService() { //this is how we retrieve our users from our database
+        UserDetails pepeUser = User.builder()
+                .username("pepe")
+                .password(passwordEncoder.encode("123")) // Encoded password does not look like BCrypt -> this is what happens without passwordEncoder
+                .roles("MEMBER") //ROLE_MEMBER
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                pepeUser
+        );
     }
 }
